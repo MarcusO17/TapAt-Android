607469bc8adba67f7118ca5a1dbc4a2e26196afa
MarcusO17 <marcusonggeye@gmail.com>
2023-09-07 17:44:02 +0800

    added nfc detection (active tag)

--- app/src/main/java/com/example/tapat/MainActivity.java
+++ app/src/main/java/com/example/tapat/MainActivity.java
@@ -2,23 +2,55 @@ package com.example.tapat;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.nfc.NfcAdapter;
 import android.os.Bundle;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    public NfcAdapter nfcAdapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        NfcAdapter nfcAdapter = NfcAdapter.getDefaultAdapter(this);
+        nfcAdapter = NfcAdapter.getDefaultAdapter(this); //Start NFC Adapter
 
+        //Test if NFC is on or off
         if(nfcAdapter != null && nfcAdapter.isEnabled()){
-            Toast.makeText(this, "NFC Available!", Toast.LENGTH_SHORT).show();
         }else{
-            Toast.makeText(this,"NFC Not Available!", Toast.LENGTH_SHORT).show();
+            finish();
         }
+    }
+
+    @Override
+    protected void onNewIntent(Intent intent) {
+        Toast.makeText(this,"NFC Intent Recieved!", Toast.LENGTH_LONG).show();
+        super.onNewIntent(intent);
+    }
+
+    @Override
+    protected void onResume() {
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.addFlags(Intent.FLAG_RECEIVER_REPLACE_PENDING);
+
+        PendingIntent pendingIntent = PendingIntent.getActivity(this,0,intent,0);
+        IntentFilter[] intentFilters = new IntentFilter[]{};
+
+        nfcAdapter.enableForegroundDispatch(this, pendingIntent, intentFilters, null);
+
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+
+        nfcAdapter.disableForegroundDispatch(this);
+
+        super.onPause();
     }
 }
