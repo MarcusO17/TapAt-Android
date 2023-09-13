c093c5f27239c47b1c3738ebdf1fd19c30d160b9
kelvinkyy <kelvinkyy03@gmail.com>
2023-09-13 21:08:09 +0800

    Basic Backend Structure for Login

--- app/src/main/java/com/example/tapat/MainActivity.java
+++ app/src/main/java/com/example/tapat/MainActivity.java
@@ -3,12 +3,39 @@ package com.example.tapat;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText emailInputField;
+    EditText passwordInputField;
+    Button buttonLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        emailInputField = (EditText) findViewById(R.id.loginEmailInput);
+        passwordInputField = (EditText) findViewById(R.id.loginPasswordInput);
+        buttonLogin = (Button) findViewById(R.id.buttonLogin);
+
+        buttonLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                /* change this to connect to the database when we implement database*/
+                if(emailInputField.getText().toString() == "1234" && passwordInputField.getText().toString() == "1234") {
+                    /* if correct send the user to the homepage*/
+
+                    /* Intent intent = new Intent(this, Homepage.class);
+                    startActivity(intent);*/
+                }
+                else {
+                    /* maybe something like a red alert telling the user login failed on the bottom of the screen*/
+                }
+            }
+        });
     }
 }
