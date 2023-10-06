4a4081668f6e3fbbbf26fe84521e072fde21559a
101wizard <jqgammers@gmail.com>
2023-10-06 14:02:14 +0800

    nfc writer
    basic recycle view

--- app/src/main/java/com/example/tapat/adminfragments/AdminNFCwriter.java
+++ app/src/main/java/com/example/tapat/adminfragments/AdminNFCwriter.java
@@ -1,16 +1,25 @@
 package com.example.tapat.adminfragments;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.tapat.R;
 
+import java.util.Arrays;
+
 public class AdminNFCwriter extends Fragment {
+
+    private ButtonListAdapter buttonListAdapter;
 
     public AdminNFCwriter() {
         // Required empty public constructor
@@ -27,8 +36,54 @@ public class AdminNFCwriter extends Fragment {
             textView.setText("AdminNFCWriter");
         }
 
-        // Initialize and handle NFC Writer related UI components and functionality here
+        String [] student = new String[]{"Abu", "Ali", "Chisa", "Murta", "Marci", "John", "Baboon", "Dill", "Chloe", "Furn"};
+
+        // Initialize RecyclerView
+        RecyclerView recyclerView = view.findViewById(R.id.recyclerView);
+        recyclerView.setLayoutManager(new LinearLayoutManager(requireContext()));
+
+        // Initialize ButtonListAdapter
+        buttonListAdapter = new ButtonListAdapter(requireContext());
+
+        // Set the adapter for the RecyclerView
+        recyclerView.setAdapter(buttonListAdapter);
+
+        // Initialize views
+        EditText searchEditText = view.findViewById(R.id.searchEditText);
+
+        buttonListAdapter.setOnItemClickListener(new ButtonListAdapter.OnItemClickListener() {
+            @Override
+            public void onItemClick(String buttonText) {
+                writeNfc(buttonText);
+            }
+        });
+
+        // Initialize and populate the button list
+        buttonListAdapter.setData(Arrays.asList(student));
+
+        // Implement search functionality
+        searchEditText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+                // Not needed in this case
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                // Filter the button list based on the search text
+                buttonListAdapter.getFilter().filter(s);
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                // Not needed in this case
+            }
+        });
 
         return view;
+    }
+
+    private void writeNfc(String buttonName){
+        //nfc function
     }
 }
