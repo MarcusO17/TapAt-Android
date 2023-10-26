2f0f9cceab506d3385d95e611221b673c6defe8b
kelvinkyy <kelvinkyy03@gmail.com>
2023-10-26 17:12:44 +0800

    Ask for Permission

--- app/src/main/java/com/example/tapat/LoginActivity.java
+++ app/src/main/java/com/example/tapat/LoginActivity.java
@@ -1,15 +1,21 @@
 package com.example.tapat;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 import androidx.core.os.EnvironmentCompat;
 
 import com.example.tapat.helpers.dbHelper;
+
+import android.Manifest;
+import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Color;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Environment;
 import android.util.Log;
@@ -17,6 +23,8 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+
+import java.io.IOException;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -27,12 +35,15 @@ public class LoginActivity extends AppCompatActivity {
     Button buttonLogin;
     dbHelper db;
     String sessionID;
-    private static final String STORAGE_PERMISSION_REQUEST_CODE = "101";
+    private static final int STORAGE_PERMISSION_REQUEST_CODE = 101;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_activity);
+
+        int currentApiVersion = android.os.Build.VERSION.SDK_INT;
+        Log.d("CURRENT API LEVEL", currentApiVersion + "");
 
         emailInputField = (EditText) findViewById(R.id.loginIDInput);
         passwordInputField = (EditText) findViewById(R.id.loginPasswordInput);
@@ -40,30 +51,30 @@ public class LoginActivity extends AppCompatActivity {
         db = new dbHelper(this);
 
         buttonLogin.setOnClickListener(new View.OnClickListener() {
-        @Override
+            @Override
             public void onClick(View view) {
                 email = emailInputField.getText().toString();
                 password = passwordInputField.getText().toString();
                 /* change this to connect to the database when we implement database*/
-                    /* if correct send the user to the homepage*/
-                sessionID  = db.userAuthorization(email,password);
-                if(sessionID.length()>0) {
-                    if(sessionID.startsWith("A")){
+                /* if correct send the user to the homepage*/
+                sessionID = db.userAuthorization(email, password);
+                if (sessionID.length() > 0) {
+                    if (sessionID.startsWith("A")) {
                         Intent intent = new Intent(getApplicationContext(), AdminActivity.class);
-                        intent.putExtra("sessionID",sessionID);
+                        intent.putExtra("sessionID", sessionID);
                         startActivity(intent);
                     }
-                    if(sessionID.startsWith("L")){
+                    if (sessionID.startsWith("L")) {
                         Intent intent = new Intent(getApplicationContext(), FragmentHolderActivity.class);
-                        intent.putExtra("sessionID",sessionID);
+                        intent.putExtra("sessionID", sessionID);
                         startActivity(intent);
                     }
-                }else {
+                } else {
                     /* toast to alert the user that the login has failed*/
                     String errorText = "";
-                    if(email.equals("")) {
+                    if (email.equals("")) {
                         errorText = "Login Failed: Invalid Credentials";
-                    } else if (password .equals("")) {
+                    } else if (password.equals("")) {
                         errorText = "Login Failed: Invalid Password";
                     }
                     Toast loginToastError = Toast.makeText(view.getContext(), errorText, Toast.LENGTH_SHORT);
@@ -73,5 +84,21 @@ public class LoginActivity extends AppCompatActivity {
                 }
             }
         });
+    }
+    public boolean checkPermissionForReadExtertalStorage() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            int result = this.checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE);
+            return result == PackageManager.PERMISSION_GRANTED;
+        }
+        return false;
+    }
+    public void requestPermissionForReadExtertalStorage() throws Exception {
+        try {
+            ActivityCompat.requestPermissions((Activity) this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                    STORAGE_PERMISSION_REQUEST_CODE);
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw e;
+        }
     }
 }
