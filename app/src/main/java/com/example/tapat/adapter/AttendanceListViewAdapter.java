ceba8f954de6e21a5bce32c2ba73fde98ba59c5a
kelvinkyy <kelvinkyy03@gmail.com>
2023-11-07 13:02:53 +0800

    Attendance Checkbox Confirmation Button Implemented

--- app/src/main/java/com/example/tapat/adapter/AttendanceListViewAdapter.java
+++ app/src/main/java/com/example/tapat/adapter/AttendanceListViewAdapter.java
@@ -1,5 +1,8 @@
 package com.example.tapat.adapter;
 
+import static java.security.AccessController.getContext;
+
+import android.content.DialogInterface;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -10,13 +13,18 @@ import android.widget.Spinner;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
+import androidx.fragment.app.FragmentManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.tapat.R;
 import com.example.tapat.model.AttendanceListRowData;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Calendar;
 import java.util.List;
 
 public class AttendanceListViewAdapter extends RecyclerView.Adapter<AttendanceListViewAdapter.ViewHolder>{
@@ -75,11 +83,40 @@ public class AttendanceListViewAdapter extends RecyclerView.Adapter<AttendanceLi
             dropDownBox = itemView.findViewById(R.id.reasondropdownbox);
             attendanceCheckBox = itemView.findViewById(R.id.attendancecheckbox);
 
-            attendanceCheckBox.setOnCheckedChangeListener((checkbox, isChecked) ->{
-                int adapterPosition = getAdapterPosition();
-                if (adapterPosition != RecyclerView.NO_POSITION) {
-                    AttendanceListRowData rowData = attendanceList.get(adapterPosition);
-                    rowData.setAttendance(isChecked);
+            attendanceCheckBox.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    int adapterPosition = getAdapterPosition();
+                    if (adapterPosition != RecyclerView.NO_POSITION) {
+                        AlertDialog.Builder builder = new AlertDialog.Builder(itemView.getContext());
+                        builder.setCancelable(true);
+
+                        if (!attendanceCheckBox.isChecked()) {
+                            builder.setMessage("Do you want to uncheck this attendance?");
+                        } else {
+                            builder.setMessage("Do you want to check this attendance?");
+                        }
+
+                        builder.setPositiveButton("Confirm", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int id) {
+                                dialog.dismiss();
+                            }
+                        });
+
+                        builder.setNegativeButton(android.R.string.cancel, new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int id) {
+
+                                attendanceCheckBox.setChecked(!attendanceCheckBox.isChecked());
+                                AttendanceListRowData rowData = attendanceList.get(adapterPosition);
+                                rowData.setAttendance(attendanceCheckBox.isChecked());
+                            }
+                        });
+
+                        AlertDialog dialog = builder.create();
+                        dialog.show();
+                    }
                 }
             });
 
